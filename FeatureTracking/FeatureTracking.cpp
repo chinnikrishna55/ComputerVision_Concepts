f872d47088f1196576a10296e3ab28b9e0def1ff
chinnikrishna55 <krishnamohan.chinni@gmail.com>
2016-03-10 01:00:42 -0800

    Adding FPS info.

--- FeatureTracking/FeatureTracking.cpp
+++ FeatureTracking/FeatureTracking.cpp
@@ -11,17 +11,22 @@ int main(int, char**)
         return -1;
     cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
     cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
+    Ptr<AKAZE> akaze = AKAZE::create(AKAZE::DESCRIPTOR_MLDB, 64, 1, 0.001f, 2, 2,KAZE::DIFF_PM_G2);
     namedWindow("Video Stream",1);
     for(;;)
     {
         Mat frame;
         cap >> frame; // get a new frame from camera
-	Ptr<AKAZE> akaze = AKAZE::create(AKAZE::DESCRIPTOR_MLDB, 128, 1, 0.001f, 4, 4,KAZE::DIFF_PM_G2);
+	double t = (double)getTickCount();
 	vector<KeyPoint> kpts1, kpts2;
 	Mat desc1, desc2;
 	akaze->detectAndCompute(frame, noArray(), kpts1, desc1);
+	t = ((double)getTickCount() - t)/getTickFrequency();
         drawKeypoints( frame, kpts1, frame, Scalar(0,0,255), DrawMatchesFlags::DEFAULT );
-
+	char text[100];
+	sprintf(text,"FPS: %lf", 1/t);
+	putText(frame, text, cvPoint(30,30),
+	    FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200,200,250), 1, CV_AA);
 	imshow("Video Stream", frame);
         if(waitKey(30) >= 0) break;
     }
